c3882e35a5445f4aab78e510149434ef5c6b22f4
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-09 01:30:51 +0100

    add attestation create component

---
+++ resources/views/components/attestation-create.blade.php
@@ -0,0 +1,68 @@
+<div id="modal" tabindex="-1" aria-hidden="true"
+    class="hidden mx-auto overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center max-w-7xl md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Nouvelle Catégorie
+                </h3>
+                <button type="button"
+                    class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5">
+                <div class="max-h-auto mx-auto max-w-2xl">
+                    @if ($errors->any())
+                        <div class="bg-red-500 text-white px-3 py-2 rounded-lg mb-4">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form class="w-full" action="{{ route('attestations.store') }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div>
+                            <x-input-label for="student_name" :value="__('Nom')" />
+                            <x-text-input id="student_name" class="block mt-1 w-full" type="text" name="student_name"
+                                :value="old('student_name')" required autofocus autocomplete="student_name" />
+                            <x-input-error :messages="$errors->get('student_name')" class="mt-2" />
+                        </div>
+                        <div class="mt-2">
+                            <x-input-label for="file" :value="__('Attestations')" />
+                            <x-text-input
+                                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                aria-describedby="file" name="file" id="file" type="file" />
+                                <x-input-error :messages="$errors->get('file')" class="mt-2" />
+                        </div>
+                        <div class="mt-2">
+                            <x-input-label for="image" :value="__('Image')" />
+                            <x-text-input
+                                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                aria-describedby="image" name="image" id="image" type="file" />
+                                <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                        </div>
+                        <div class="flex justify-start mt-2 mb-4">
+                            <button type="submit"
+                                class="text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">
+                                Envoyer
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
