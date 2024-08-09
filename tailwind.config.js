ab1e8201a017b66b1e6ad01432caff9eb3171ee7
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-09 01:30:17 +0100

    installation de flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,22 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.vue",
+        "./resources/**/*.js",
+        "./node_modules/flowbite/**/*.js"
+
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree','raleway', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
