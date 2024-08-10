265a74a5c32589dc7a017c511663ef1830a43755
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-10 09:09:31 +0100

    update color of cancel button on delete modal

--- resources/views/components/delete.blade.php
+++ resources/views/components/delete.blade.php
@@ -28,7 +28,7 @@
                         Oui
                     </button>
                     <button data-modal-hide="delete-modal" type="button"
-                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Non</button>
+                        class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-gray-800 rounded-lg border border-gray-200 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100">Non</button>
                 </form>
             </div>
         </div>
