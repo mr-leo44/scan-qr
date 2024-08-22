0d514b64f2aefc2c76ab2029f1702f1227de5e97
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-22 07:58:12 +0100

    improve footer layout after updating tailwind config

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,16 @@
-    <div class="bg-footer-bg py-4">
+    <div class="bg-footer py-4">
         <p class="px-8 text-white text-lg uppercase font-bold mt-10">contenu</p>
         <div class="px-8 text-white font-bold text-lg mt-8 flex flex-col">
             <a href="https://www.schoolap.com/lecons" class="my-1">Leçons</a>
             <a href="https://www.schoolap.com/exetats" class="my-1"> Item-Exetat</a>
             <a href="https://www.schoolap.com/fiches" class="my-1">Fiche</a>
         </div>
-        <div class="mt-28 py-3 text-center text-white">©2023 | Tous droits reservés.</div>
+        @php
+            use Carbon\Carbon;
+        @endphp
+        <div class="mt-28 py-3 text-center text-white">©{{ Carbon::now()->year }} | Tous droits reservés.</div>
 
     </div>
     </body>
-</html>
+
+    </html>
